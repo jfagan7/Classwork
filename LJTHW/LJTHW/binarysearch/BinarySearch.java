c800f9411a5355dd2bec8c7de6109061ea16cd59
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-15 12:05:06 -0500

    binary search 2

---
+++ LJTHW/LJTHW/binarysearch/BinarySearch.java
@@ -0,0 +1,39 @@
+package binarysearch;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] test1= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		int[] test2= {0, 1, 2, 3, 4, 5, 6, 7, 8};
+		
+		for(int i=-1;i<test1.length+1;i++)
+		{
+			System.out.print(binarySearch(test1,i,0,test1.length));
+		}
+		System.out.println();
+		for(int i=-1;i<test2.length+1;i++)
+		{
+			System.out.print(binarySearch(test2,i,0,test2.length));
+		}
+	}
+	public static int binarySearch(int[]nums, int query,int first, int last)
+	{
+		if(last>=first)
+		{
+			int index= (first+last)/2;
+			int guess= nums[index];
+			
+			if(guess==query)
+			{
+				return index;
+			}
+			if(guess>query)
+			{
+				return binarySearch(nums,query,first,index-1);
+			}
+			return binarySearch(nums,query,index+1,last);
+		}
+		return -1;
+	}
+}
